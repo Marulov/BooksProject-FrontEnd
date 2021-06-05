92f192ec95e59b932935192f799929a9dd0cf54a
Nazım Marul <nazmmarul@hotmail.com>
2021-06-05 11:58:03 +0300

    Proje sonu

---
+++ src/components/BookList.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './BookList.css';
+import Book from './Book';
+import  { BookContext } from '../contexts/BookContext';
+import  { ThemeContext } from '../contexts/ThemeContext';
+
+class BookList extends React.Component {
+
+
+    render() {
+        return (
+
+            <ThemeContext.Consumer>{(contextTheme) => (
+                <BookContext.Consumer>
+                    {contextBook => {
+                    
+                    const { books } = contextBook;
+                    const {changeTheme, isDarkTheme, dark, light } = contextTheme;
+                    const theme = isDarkTheme ? dark : light;
+                
+                    return (
+                        <section className="page-section" style={{background: theme.bg, color:theme.txt}} id="portfolio">
+                        <div className="container">
+                            <div className="text-center">
+                                <h2 className="section-heading text-uppercase">BookFolio</h2>
+                                <h3 className="section-subheading text-muted">Sadece kitap değil macera alıyorsunuz .</h3>
+                                <button type="button" className="btn btn-sm btn-info" style={{marginTop: '-70px'}}
+                                onClick={changeTheme}>Change Theme</button>
+                            </div>
+                            <div className="row">
+        
+                                {books.map((book, i) => {
+                                    return <Book book={book}
+                                        key={i}
+                                    />
+                                })}
+        
+                            </div>
+                        </div>
+                    </section>
+                    )
+                }}
+            </BookContext.Consumer>
+            )}
+
+
+            </ThemeContext.Consumer>
+
+
+
+
+        )
+    }
+
+}
+
+export default BookList;
+
+
